4ce1cee10773e5d2664dc46b6ef15f045863d5b6
anthony stachowitz <42899766+stackBlock@users.noreply.github.com>
2019-06-24 04:24:16 -0400

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const usersRoute = require('./users.js');
 const storiesRoute = require('./stories.js');
 
-// router.use('/users', usersRoute);
+router.use('/users', usersRoute);
 router.use('/stories', storiesRoute);
 
 module.exports = router;
